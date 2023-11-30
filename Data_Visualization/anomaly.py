5df775f26f01e1146394106fa0759558d461e0f1
Ben Makhlouf <makhlobe@gmail.com>
2023-11-29 16:17:42 -0800

    Recreated multiple functions to handle various parts of time series analy.

--- Data_Visualization/anomaly.py
+++ Data_Visualization/anomaly.py
@@ -1,51 +1,156 @@
-"This script defines a function to calculate anomolies in the data"
 import pandas as pd
+import plotly.io as pio
 import pytimetk
 
-# Load data
-data = pd.read_csv('/Users/benjaminmakhlouf/Desktop/Merged.csv')
-data['Time'] = pd.to_datetime(data['Time'])
+data = pd.read_csv('/Users/benjaminmakhlouf/Desktop/daily_tidy_all_data.csv')
+data['times'] = pd.to_datetime(data['times'])
+data = data[data['parameter'] == 'Air_Temperature']
+data = data.dropna(subset=['value_mean']).query('value_mean != 0')
 
-# Filter data to include only the "Temperature" variable
-data = data[data['variable'] == 'Relative Humidity']
+print(data)
 
 
-def calculate_and_plot_anomalies(data, date_column, value_column, period, iqr_alpha, clean_alpha, clean, engine, title):
+#### Data should be queried before it gets to the functions 
+
+def create_trendline(data):
     """
-    This function calculates and plots anomalies in time series data.
-    Inputs: Time series data with one variable chosen,
-            date_column: the column name for the date/time variable,
-            value_column: the column name for the variable to be analyzed,
-            period: the period of the time series (e.g. 7 for weekly, 12 for monthly),
-            iqr_alpha: the alpha value for the interquartile range,
-            clean_alpha: the alpha value for the cleaning,
-            clean: the type of cleaning (e.g. 'min_max'),
-            engine: the plotting engine (e.g. 'plotly'),
-            title: the title of the plot.
-    Outputs: Two plots, one of the seasonal decomposition and one of the anomalies.
+    Create a trendline plot for the given data.
+
+    Args:
+        data (pandas.DataFrame): The input data containing the 'times' and 'value_mean' columns.
+
+    Raises:
+        ValueError: If the 'times' or 'value_mean' column does not exist in the data.
+        ValueError: If the 'times' column is not a datetime object.
+        ValueError: If there is no data in either the 'times' or 'value_mean' column.
+        ValueError: If there is a non-numeric value in the 'value_mean' column.
+
+    Returns:
+        None
     """
+    if 'times' not in data.columns or 'value_mean' not in data.columns:
+        raise ValueError("The 'times' or 'value_mean' column does not exist in the data.")
+
+    if data['times'].dtype != 'datetime64[ns]':
+        raise ValueError("The 'times' column is not a datetime object.")
+
+    if data['times'].empty or data['value_mean'].empty:
+        raise ValueError("There is no data in either the 'times' or 'value_mean' column.")
+
+    if not pd.to_numeric(data['value_mean'], errors='coerce').notnull().all():
+        raise ValueError("There is a non-numeric value in the 'value_mean' column.")
+
+    fig = pytimetk.plot_timeseries(
+        data=data,
+        date_column='times',
+        value_column='value_mean'
+    )
+    pio.show(fig)
+
+
+create_trendline(data)
+
+
+
+
+
+def create_anomaly_graph(data):
+    """
+    Create an anomaly graph with anomaly detection and plot.
+
+    Args:
+        data (pandas.DataFrame): The input data containing the 'times' and 'value_mean' columns.
+
+    Raises:
+        ValueError: If the 'times' or 'value_mean' column does not exist in the data.
+        ValueError: If the 'times' column is not a datetime object.
+        ValueError: If there is no data in either the 'times' or 'value_mean' column.
+        ValueError: If there is a non-numeric value in the 'value_mean' column.
+
+    Returns:
+        None
+    """
+    if 'times' not in data.columns or 'value_mean' not in data.columns:
+        raise ValueError("The 'times' or 'value_mean' column does not exist in the data.")
+
+    if data['times'].dtype != 'datetime64[ns]':
+        raise ValueError("The 'times' column is not a datetime object.")
+
+    if data['times'].empty or data['value_mean'].empty:
+        raise ValueError("There is no data in either the 'times' or 'value_mean' column.")
+
+    if not pd.to_numeric(data['value_mean'], errors='coerce').notnull().all():
+        raise ValueError("There is a non-numeric value in the 'value_mean' column.")
+
     anomalize_df = pytimetk.anomalize(
         data=data,
-        date_column=date_column,
-        value_column=value_column,
-        period=period,
-        iqr_alpha=iqr_alpha,  # using the default
-        clean_alpha=clean_alpha,  # using the default
-        clean=clean
+        date_column='times',
+        value_column='value_mean',
+        period=7,
+        iqr_alpha=0.05,
+        clean_alpha=0.75,
+        clean="min_max"
     )
 
-    fig1 = pytimetk.plot_anomalies_decomp(
+    anomplot = pytimetk.plot_anomalies(
         data=anomalize_df,
-        date_column=date_column,
-        engine=engine,
-        title=title
+        date_column='times',
+        engine='plotly',
+        title='Plot Anomaly Bands'
     )
 
-    fig2 = pytimetk.plot_anomalies(
-        data=anomalize_df,
-        date_column=date_column,
-        engine=engine,
-        title=title
+    pio.show(anomplot)
+
+
+create_anomaly_graph(data)
+
+
+
+### Show the statistical decomposition of the anamoly graph 
+
+def create_anomaly_graph(data):
+    """
+    Create an anomaly graph with anomaly detection and plot.
+
+    Args:
+        data (pandas.DataFrame): The input data containing the 'times' and 'value_mean' columns.
+
+    Raises:
+        ValueError: If the 'times' or 'value_mean' column does not exist in the data.
+        ValueError: If the 'times' column is not a datetime object.
+        ValueError: If there is no data in either the 'times' or 'value_mean' column.
+        ValueError: If there is a non-numeric value in the 'value_mean' column.
+
+    Returns:
+        None
+    """
+    if 'times' not in data.columns or 'value_mean' not in data.columns:
+        raise ValueError("The 'times' or 'value_mean' column does not exist in the data.")
+
+    if data['times'].dtype != 'datetime64[ns]':
+        raise ValueError("The 'times' column is not a datetime object.")
+
+    if data['times'].empty or data['value_mean'].empty:
+        raise ValueError("There is no data in either the 'times' or 'value_mean' column.")
+
+    if not pd.to_numeric(data['value_mean'], errors='coerce').notnull().all():
+        raise ValueError("There is a non-numeric value in the 'value_mean' column.")
+
+    anomalize_df = pytimetk.anomalize(
+        data=data,
+        date_column='times',
+        value_column='value_mean',
+        period=7,
+        iqr_alpha=0.05,
+        clean_alpha=0.75,
+        clean="min_max"
     )
 
-    return fig1, fig2
+    decomp=pytimetk.plot_anomalies_decomp(
+    data        = anomalize_df,
+    date_column = 'times',
+    engine      = 'plotly',
+    title       = 'Seasonal Decomposition'
+)
+
+pio.show(decomp)
