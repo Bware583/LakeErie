21a2df32f99f9cf3f4434133e15927e3c5e68c6c
Ben Makhlouf <makhlobe@gmail.com>
2023-12-04 15:05:29 -0800

    double checked and slightly updated tests for map

--- test/frontend/test_map.py
+++ test/frontend/test_map.py
@@ -3,34 +3,30 @@ from leaflet_map import map_main, map_main_with_custom_buoys
 
 class TestMapMainFunction(unittest.TestCase):
 
-    def test_valid_buoy(self):
-        # Test with a valid buoy
+    def test_valid_selected_buoy(self):
+        # Test with a valid selected buoy
         selected_buoy = "Buoy 3"
-        # This test should not raise any exceptions
-        map_html = map_main(selected_buoy)
-        self.assertIsInstance(map_html, str)
+        result = map_main(selected_buoy)
+        self.assertIsInstance(result, folium.Map)
 
-    def test_invalid_buoy(self):
-        # Test with an invalid buoy
+    def test_invalid_selected_buoy(self):
+        # Test with an invalid selected buoy
         selected_buoy = "Invalid Buoy"
-        # Expecting a ValueError to be raised since the buoy is not in the predefined list
-        with self.assertRaises(ValueError):
+        with self.assertRaises(ValueError) as context:
             map_main(selected_buoy)
+        self.assertEqual(str(context.exception), "Invalid selected buoy 'Invalid Buoy'. Please select a valid buoy from the list.")
 
-    def test_non_numeric_coordinates(self):
-        # Test with non-numeric coordinates for a buoy
+    def test_invalid_coordinates(self):
+        # Test with invalid coordinates (non-numeric)
         buoy_locations = [
-            {"name": "Invalid Buoy", "latitude": "invalid", "longitude": 2.3522},
+            {"name": "Invalid Buoy", "latitude": "invalid", "longitude": "invalid"}
         ]
-        # Expecting a ValueError to be raised since latitude is not numeric
-        with self.assertRaises(ValueError):
-            map_main_with_custom_buoys("Invalid Buoy", buoy_locations)
+        with self.assertRaises(ValueError) as context:
+            map_main("Buoy 3")
+        self.assertEqual(str(context.exception), "Invalid coordinates for buoy 'Invalid Buoy'. Latitude and longitude must be numeric.")
 
-    def test_invalid_coordinates_type(self):
-        # Test with coordinates of invalid type
-        buoy_locations = [
-            {"name": "Invalid Buoy", "latitude": "invalid", "longitude": 2.3522},
-        ]
-        # Expecting a ValueError to be raised since latitude is not numeric
-        with self.assertRaises(ValueError):
-            map_main_with_custom_buoys("Invalid Buoy", buoy_locations)
+    def test_valid_coordinates(self):
+        # Test with valid coordinates (numeric)
+        selected_buoy = "Buoy 3"
+        result = map_main(selected_buoy)
+        self.assertIsInstance(result, folium.Map)
